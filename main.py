b167e4044cf0c64805f865225d5c9b964ccc54bd
axhutosh <96404572+axhutosh@users.noreply.github.com>
2023-04-13 14:44:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("hello jaan")
 name=input("enter your name")
 time=float(input("enter the time"))
-if(time<=12)
-print("good morning", name)
-elif("time>12")
-print("good afternoon", name)
+if(time<=12):
+  print("good morning", name)
+elif(time>12):
+  print("good afternoon", name)
